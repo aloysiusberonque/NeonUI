03577bd284467b1f6ac511af5a5ebf21fea3812c
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2023-02-24 14:49:45 +0800

    Implemented background for onboarding screen

---
+++ neon/lib/screens/onboarding_screen.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Constants.kBlackColor,
+      extendBody: true,
+      body: SizedBox(
+        height: screenHeight,
+        width: screenWidth,
+        child: Stack(
+          children: [
+            Positioned(
+                top: screenHeight * 0.1,
+                left: -88,
+                child: Container(
+                  height: 166,
+                  width: 166,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Constants.kPinkColor),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                    child: Container(
+                      height: 166,
+                      width: 166,
+                    ),
+                  ),
+                )),
+            Positioned(
+                top: screenHeight * 0.3,
+                right: -100,
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Constants.kGreenColor),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                    ),
+                  ),
+                )),
+            SafeArea(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: screenHeight*0.07,
+                )
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
